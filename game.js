c0405df8b8afe7d3ba1340c44c4dfd40b2f6de8b
Gabriel Tarcia <gabriel.liminha2025@gmail.com>
2026-05-14 16:50:21 -0300

    projéteis, movimentação do inimigo, colisão com a bala e o inimigo com as bordas

--- game.js
+++ game.js
@@ -21,11 +21,14 @@ const game = new Phaser.Game(config);
 let player;
 let cursors;
 let enemy;
+let bullets;
 
 function preload(){
     // Carregar uma imagem (sprite do personagem)
     this.load.image('player', 'ig11.png');
-    this.load.image('enemy', 'mandalorian.png');;
+    this.load.image('enemy', 'mandalorian.png');
+    this.load.image('enemyBullet', 'blue.png');
+    this.load.image('playerBullet', 'red.png');
 }
 
 function create(){
@@ -44,11 +47,25 @@ function create(){
     // Reduzindo o tamanho do inimigo para 50% (0.5x)
     enemy.setScale(0.5);
 
+     // Colisão do inimigo
+    enemy.setCollideWorldBounds(true);
+    enemy.setBounce(1);
+
+    enemy.setVelocityY(550);
+
+    bullets = this.physics.add.group();
+    this.time.addEvent({
+        delay: 2000,
+        callback: shootBullet,
+        callbackScope: this,
+        loop: true,
+    })
+
+    this.physics.add.overlap(player, bullets, hitPlayer, null, this);
+
     // Criando controles de teclado
     cursors = this.input.keyboard.createCursorKeys();
 
-    // Colisão do inimigo
-    enemy.setCollideWorldBounds(true);
 }
 
 function update(){
@@ -58,7 +75,20 @@ function update(){
     } else if (cursors.down.isDown) {
         player.y += 3;
     }
+}
 
-    // Movimentação do inimigo
-    
+function shootBullet(){
+    let bullet = bullets.create(enemy.x -50, enemy.y, "enemyBullet");
+    bullet.setScale(0.2)
+    bullet.body.allowGravity = false;
+    bullet.setVelocityX(-400);
+}
+
+function hitPlayer(player, bullet){
+    bullet.destroy();
+    player.setTint(0xff0000);
+    console.log("Jogador atingido!!")
+    setTimeout(() => {
+        player.clearTint();
+    }, 200)
 }
